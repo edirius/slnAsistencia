df1801dae329cf34d2ce5c5ee1b0593fa94c8724
OMAR ESTEBAN SICCOS ESTRADA <oese322@hotmail.com>
2017-04-05 14:48:55 -0500

    05/04/2017

---
+++ CapaPresentacion/CronogramaVacaciones/wListaCronogramaVacaciones.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace CapaPresentacion.CronogramaVacaciones
+{
+    /// <summary>
+    /// Lógica de interacción para wListaCronogramaVacaciones.xaml
+    /// </summary>
+    public partial class wListaCronogramaVacaciones : Window
+    {
+        public wListaCronogramaVacaciones()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ CapaPresentacion/DiasFestivos/wListaDiasFestivos.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace CapaPresentacion.DiasFestivos
+{
+    /// <summary>
+    /// Lógica de interacción para wListaDiasFestivos.xaml
+    /// </summary>
+    public partial class wListaDiasFestivos : Window
+    {
+        public wListaDiasFestivos()
+        {
+            InitializeComponent();
+        }
+    }
+}
