c8193dfb77cbead68393bbbe41811d47f93e5c05
Edward <edirius@gmail.com>
2017-04-05 12:30:00 -0500

    Formularios

---
+++ CapaPresentacion/AgenciaAgraria/wAgenciaAgraria.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using CapaDeNegocios;
+using CapaEntities;
+
+namespace CapaPresentacion.AgenciaAgraria
+{
+    /// <summary>
+    /// Interaction logic for wAgenciaAgraria.xaml
+    /// </summary>
+    public partial class wAgenciaAgraria : Window
+    {
+        public Local miLocal;
+
+        public wAgenciaAgraria()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            Iniciar();
+        }
+
+        private void Iniciar()
+        {
+            txtNombre.Text = miLocal.Nombre;
+        }
+
+        private void btnAceptar_Click(object sender, RoutedEventArgs e)
+        {
+            miLocal.Nombre = txtNombre.Text;
+            this.DialogResult = true;
+        }
+
+        private void btnCancelar_Click(object sender, RoutedEventArgs e)
+        {
+            DialogResult = false;
+        }
+    }
+}
---
+++ CapaPresentacion/Entidad/wListaEntidad.xaml.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using CapaDeNegocios;
+using CapaEntities;
+
+
+namespace CapaPresentacion.Entidad
+{
+    /// <summary>
+    /// Interaction logic for wListaEntidad.xaml
+    /// </summary>
+    public partial class wListaEntidad : Window
+    {
+        public Empresa miEntidad;
+        CapaDeNegocios.blLocal.blLocal oblLocal = new CapaDeNegocios.blLocal.blLocal();
+
+        public wListaEntidad()
+        {
+            InitializeComponent();
+            
+        }
+
+        public void Iniciar()
+        {
+            ICollection<Local> ListaLocales = oblLocal.ListarLocales();
+            txtNombreEntidad.Text = miEntidad.Nombre;
+            
+            dtgAgenciasAgrarias.ItemsSource = ListaLocales ;
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            Iniciar();
+        }
+
+        private void btnGuardar_Click(object sender, RoutedEventArgs e)
+        {
+            AgenciaAgraria.wAgenciaAgraria fAgenciaAgraria = new AgenciaAgraria.wAgenciaAgraria();
+            fAgenciaAgraria.miLocal = new Local();
+            fAgenciaAgraria.miLocal.Empresa = miEntidad; 
+            if (fAgenciaAgraria.ShowDialog() == true)
+            {
+                oblLocal.AgregarLocal(fAgenciaAgraria.miLocal); 
+            }
+            
+        }
+
+        private void btnModificarAGencia_Click(object sender, RoutedEventArgs e)
+        {
+            AgenciaAgraria.wAgenciaAgraria fAgenciaAgraria = new AgenciaAgraria.wAgenciaAgraria();
+            fAgenciaAgraria.miLocal = (Local) dtgAgenciasAgrarias.SelectedItem ;
+            if (fAgenciaAgraria.ShowDialog() == true)
+            {
+                oblLocal.ModificarLocal(fAgenciaAgraria.miLocal);
+            }
+        }
+    }
+}
